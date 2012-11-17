3c3ff5abefb4d5d7c4c4d5bb9bca44004c457d20
Tanja Pislar <tanja.pislar@gmail.com>
2012-11-17 11:57:09 +0000

    hash shoudl be outside module

--- lib/brio/utils.rb
+++ lib/brio/utils.rb
@@ -1,9 +1,8 @@
-module Brio
-  class Hash
-    def method_missing(name)
-      return self[name] if key? name
-      self.each { |k,v| return v if k.to_s.to_sym == name }
-      super.method_missing name
-    end
+
+class Hash
+  def method_missing(name)
+    return self[name] if key? name
+    self.each { |k,v| return v if k.to_s.to_sym == name }
+    super.method_missing name
   end
 end
