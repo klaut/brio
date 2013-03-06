2221e1cbb8aa5b76cb863c7ac7d9034169d50548
Parker Moore <parkrmoore@gmail.com>
2013-03-06 22:00:02 +0100

    Add Post#normalized_text

--- lib/brio/resources/post.rb
+++ lib/brio/resources/post.rb
@@ -1,9 +1,11 @@
 module Brio
   module Resources
-
     class Post < Resource
       attr_accessor :id, :user, :created_at, :text, :num_replies, :num_stars, :num_reposts, :reply_to, :repost_of
-    end
 
+      def normalized_text
+        text.gsub(/\r\n/, "\n").gsub(/\n\n/, "\n").gsub(/\n/, " ").strip
+      end
+    end
   end
 end
